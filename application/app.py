348bd17cbaddc05b83ecc57500ff49ffa667c963
adam king <kingab_93@yahoo.com>
2020-08-10 07:29:41 -0500

    Added outlines
    ;
    q
    quit
    ;
    ;;;
    s;alksjdflkjasdl;jfa;sdljf
    
    /s
    /q
    \q

--- application/app.py
+++ application/app.py
@@ -23,3 +23,109 @@ toolbar = DebugToolbarExtension(app)
 
 crisis_connect_db(app)
 social_connect_db(app)
+
+########################## Login/logout/register routes ###############################
+
+@app.before_request
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+        return True
+    
+    return False
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    """Handle user signup.
+
+    Create new user and add to DB. Redirect to home page.
+
+    If form not valid, present form.
+
+    If the there already is a user with that username: flash message
+    and re-present form.
+    """
+
+    form = UserAddForm()
+
+    if form.validate_on_submit():
+        try:
+            user = User.signup(
+                username=form.username.data,
+                password=form.password.data,
+            )
+            db.session.commit()
+
+        except IntegrityError:
+            flash("Username already taken", 'danger')
+            return render_template('users/signup.html', form=form)
+
+        do_login(user)
+
+        return redirect("/")
+
+    else:
+        return render_template('users/signup.html', form=form)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """Handle user login."""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.authenticate(form.username.data,
+                                 form.password.data)
+
+        if user:
+            do_login(user)
+            flash(f"Hello, {user.username}!", "success")
+            return redirect("/")
+
+        flash("Invalid credentials.", 'danger')
+
+    return render_template('users/login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """Handle logout of user."""
+
+    # IMPLEMENT THIS
+    if do_logout():
+        flash(f"Log Out Successful!", "success")
+        return redirect("/login")
+
+    
+    return redirect("/")
+
+######################################### User profile/community routes #############################
+
+@app.route('/users')
+
+@app.route('/users/<user_id>')
+
+@app.route('/user/posts')
+
+
+######################################## Depression info routes #######################################
+
+@app.route('/depression')
+
+@app.route('/depression/referrals')
+
+@app.route('/depression/treatment-info')
+
+
+######################################## Cheer me up routes ################################################
+
+@app.route('/cheer-me-up')
