e3d3c916d477675803b3372868311f2cc3db67d6
adam king <kingab_93@yahoo.com>
2020-09-02 12:15:06 -0500

    Lots of changes

--- application/forms.py
+++ application/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SelectField
+from wtforms import StringField, PasswordField, SelectField, TextAreaField
 from wtforms.validators import DataRequired, Length, Optional
 
 
@@ -20,25 +20,25 @@ class LoginForm(FlaskForm):
 
 
 class CountyReferralForm(FlaskForm):
-    """Form for looking up local referrals"""
+    """Form for looking up local referrals by county"""
 
     county = SelectField('County', validators=[Optional()])
 
 
 class ZipReferralForm(FlaskForm):
-    """Form for looking up local referrals"""
+    """Form for looking up local referrals by zipcode"""
 
     zip_code = StringField('Zip Code', validators=[Length(max=5), Optional()])
 
 
 class PostAddForm(FlaskForm):
-    """Form for adding users."""
+    """Form for adding posts."""
 
     title = StringField('Title', validators=[DataRequired()])
-    body = PasswordField('Body', validators=[DataRequired()])
+    body = TextAreaField('Body', validators=[DataRequired()])
     
 
 class UserEditForm(FlaskForm):
-    """Form for adding users."""
+    """Form for editings users."""
 
     username = StringField('Username', validators=[DataRequired()])
