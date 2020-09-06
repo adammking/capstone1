b3ad3faea59678714a06675f466b2cc047a6b2dd
adam king <kingab_93@yahoo.com>
2020-09-06 13:35:28 -0500

    Finished adding tests for view functions

--- application/test_crisis_views.py
+++ application/test_crisis_views.py
@@ -1,32 +1,19 @@
-"""User View tests."""
-
-# run these tests like:
-#
-#    FLASK_ENV=production python -m unittest test_message_views.py
+"""Crisis View tests."""
 
 
 import os
 from unittest import TestCase
-
+from flask import session
 from crisis_models import db, crisis_connect_db, Mental_Health_Center, County, Zip_Code
 
-# BEFORE we import our app, let's set an environmental variable
-# to use a different database for tests (we need to do this
-# before we import our app, since that will have already
-# connected to the database
+
 
 os.environ['DATABASE_URL'] = "postgresql:///capstone1-test"
 
-
-# Now we can import app
 from app import app, CURR_USER_KEY
-# Create our tables (we do this here, so we only create the tables
-# once for all tests --- in each test, we'll delete the data
-# and create fresh new clean test data
+
 
 db.create_all()
-
-# Don't have WTForms use CSRF at all, since it's a pain to test
 
 app.config['WTF_CSRF_ENABLED'] = False
 
@@ -55,7 +42,7 @@ class UserViewTestCase(TestCase):
     def test_crisis_referral(self):
         with self.client as c:
             with c.session_transaction() as sess:
-                sess["responses"] = ['Yes', 'No', 'No']
+                sess["responses"] = ['Yes', 'No']
 
             resp = c.post("/crisis/answers", follow_redirects=True)
 
@@ -65,7 +52,7 @@ class UserViewTestCase(TestCase):
     def test_crisis_coping(self):
         with self.client as c:
             with c.session_transaction() as sess:
-                sess["responses"] = ['No', 'No', 'No']
+                sess["responses"] = ['No', 'No']
 
             resp = c.post("/crisis/answers", follow_redirects=True)
 
@@ -79,16 +66,9 @@ class UserViewTestCase(TestCase):
             with c.session_transaction() as sess:
                 sess["responses"] = ['No']
 
-            resp = c.get("/crisis/questions/0", follow_redirects=True)
+            resp = c.get("/crisis/questions/43", follow_redirects=True)
 
             self.assertEqual(resp.status_code, 200)
             self.assertIn("Question Error", str(resp.data))
 
-    def test_skip_with_no_answers(self):
-        with self.client as c:
-            
-
-            resp = c.get("/crisis/questions/2", follow_redirects=True)
-
-            self.assertEqual(resp.status_code, 200)
-            self.assertIn("Crisis Program", str(resp.data))
+    
--- application/test_depression_views.py
+++ application/test_depression_views.py
@@ -1,27 +1,15 @@
 """Depression View tests."""
 
-# run these tests like:
-#
-#    FLASK_ENV=production python -m unittest test_message_views.py
+
 import os
 from unittest import TestCase
 
-# BEFORE we import our app, let's set an environmental variable
-# to use a different database for tests (we need to do this
-# before we import our app, since that will have already
-# connected to the database
 
 os.environ['DATABASE_URL'] = "postgresql:///capstone1-test"
 
 
-# Now we can import app
 from app import app, CURR_USER_KEY
-# Create our tables (we do this here, so we only create the tables
-# once for all tests --- in each test, we'll delete the data
-# and create fresh new clean test data
 
-
-# Don't have WTForms use CSRF at all, since it's a pain to test
 
 app.config['WTF_CSRF_ENABLED'] = False
 
