e406480b888c60f51dac55e2b51475a8330365b1
adam king <kingab_93@yahoo.com>
2020-09-22 09:54:39 -0500

    Creating file structure

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sqlalchemy.exc import IntegrityError
 
 from crisis_program import crisis, Crisis_Program
 from crisis_models import db, crisis_connect_db, Mental_Health_Center, County, Zip_Code
-from social_models import db, social_connect_db, User, Likes, Follows, Post
+from social_models import db, social_connect_db, User, Follows, Post
 from forms import UserAddForm, LoginForm, CountyReferralForm, ZipReferralForm, PostAddForm, UserEditForm
 
 CURR_USER_KEY = "curr_user"
--- static/api-key.js
+++
@@ -1,1 +0,0 @@
-const API_KEY = "d9034aaa-493c-4306-b3ea-5e27045e5431"
