fe7ac4ce309a21ea2466b88dd05af6d01dec6fcb
adam king <kingab_93@yahoo.com>
2020-10-12 05:03:56 -0500

    Cleaned up app

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from crisis.program import crisis, Crisis_Program
 from crisis.models import crisis_db, crisis_connect_db, Mental_Health_Center, County, Zip_Code
 from users.models import social_db, social_connect_db, User, Follows, Post
 from forms import UserAddForm, LoginForm, CountyReferralForm, ZipReferralForm, PostAddForm, UserEditForm
+
+
 from depression.depression import depression_bp
 from users.users import users_bp
 from auth.auth import auth_bp
@@ -17,12 +19,14 @@ CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 
-# Get DB_URI from environ variable (useful for production/testing) or,
-# if not set there, use development local db.
+
 app.register_blueprint(depression_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(users_bp)
 app.register_blueprint(crisis_bp)
+
+# Get DB_URI from environ variable (useful for production/testing) or,
+# if not set there, use development local db.
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres:///capstone1')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -34,31 +38,13 @@ crisis_connect_db(app)
 social_connect_db(app)
 program = Crisis_Program
 mhc = Mental_Health_Center()
-########################## Login/logout/register routes ###############################
 
 
 
-
-######################################### User profile/community routes #############################
-
-
-
-
-
-######################################## Depression info routes #######################################
-
-
-
-######################################## Cheer me up routes ################################################
 
 @app.route('/cheer-me-up')
 def show_cheer_me_up():
 
     return render_template('cheer-me-up.html')
        
-   
-
-
-
-######################################## Crisis program routes ############################################
 
