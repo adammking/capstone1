c01203f5f0693cf6487f8a3a05a6db7d2f1487d2
Adam King <48133111+adammking@users.noreply.github.com>
2022-12-11 15:34:46 -0600

    updating database URL app config

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app.register_blueprint(crisis_bp)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres:///capstone1')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///capstone1')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
